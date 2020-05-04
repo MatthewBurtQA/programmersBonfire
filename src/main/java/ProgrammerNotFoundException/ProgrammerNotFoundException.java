e72654bccb3b4cd4e08d5a00c8ae324a89f24295
MatthewBurtQA <matthewaaronburt@gmail.com>
2020-05-04 12:01:19 +0100

    Added a programmer not found exception class

---
+++ src/main/java/ProgrammerNotFoundException/ProgrammerNotFoundException.java
@@ -0,0 +1,11 @@
+package ProgrammerNotFoundException;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.persistence.EntityNotFoundException;
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The Programmer doesn't exist")
+
+public class ProgrammerNotFoundException extends  EntityNotFoundException {
+}
