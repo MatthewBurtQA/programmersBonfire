65737b26e507a54dce3085b664b6b644fc50f5f4
MatthewBurtQA <matthewaaronburt@gmail.com>
2020-05-04 11:34:07 +0100

    Creating a programmer

---
+++ src/main/java/com/qa/App.java
@@ -0,0 +1,17 @@
+package com.qa;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
