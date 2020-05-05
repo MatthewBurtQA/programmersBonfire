36024edbff50f303cec8f23e80eea1fad839e2d3
MatthewBurtQA <matthewaaronburt@gmail.com>
2020-05-05 10:45:52 +0100

    added Programmer Team Repo

--- src/main/java/com/qa/repo/ProgrammerTeamRepository.java
+++ src/main/java/com/qa/repo/ProgrammerTeamRepository.java
@@ -1,4 +1,9 @@
 package com.qa.repo;
 
-public interface ProgrammerTeamRepository {
+import com.qa.domain.ProgrammerTeam;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProgrammerTeamRepository extends  JpaRepository<ProgrammerTeam, Long>{
 }
