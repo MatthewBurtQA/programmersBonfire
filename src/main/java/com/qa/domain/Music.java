a25b5e49595e6bc5d62bed239818acc468f5ff24
MatthewBurtQA <matthewaaronburt@gmail.com>
2020-05-05 10:43:25 +0100

    started work on programmer team functionality

--- src/main/java/com/qa/domain/Music.java
+++
@@ -1,7 +0,0 @@
-package com.qa.domain;
-
-
-public class Music {
-    private Long id; // sets the ID to id, generated using auto gen
-    private String name;
-}
---
+++ src/main/java/com/qa/domain/ProgrammerTeam.java
@@ -0,0 +1,42 @@
+package com.qa.domain;
+import javax.annotation.Generated;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class ProgrammerTeam {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name; // team name
+
+    @OneToMany(mappedBy = "programmerTeam", fetch = FetchType.LAZY)
+    private List<Programmer> programmers = new ArrayList<>();
+
+     public ProgrammerTeam() {
+     }
+     public ProgrammerTeam(String name){
+         this.name = name;
+     }
+     public Long getId(){
+         return id;
+     }
+
+     public void setId(Long id) {
+         this.id = id;
+     }
+    public String getName(){
+         return name;
+    }
+    public void setName(String name){
+         this.name = name;
+    }
+
+    public List<Programmer> getProgrammers(){
+         return programmers;
+    }
+
+}
