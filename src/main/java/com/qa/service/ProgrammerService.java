3ef362c7399fa0fd0144390892a4e0a962830c30
MatthewBurtQA <matthewaaronburt@gmail.com>
2020-05-06 10:55:01 +0100

    programmer DTO stuff now set up, not tested yet, need to implement team DTO first to keep up pace

--- src/main/java/com/qa/service/ProgrammerService.java
+++ src/main/java/com/qa/service/ProgrammerService.java
@@ -1,46 +1,58 @@
 package com.qa.service;
 
 import com.qa.domain.Programmer;
+import com.qa.dto.ProgrammerDTO;
 import com.qa.ProgrammerNotFoundException.ProgrammerNotFoundException;
 import com.qa.repo.ProgrammerRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class ProgrammerService {
 
     private final ProgrammerRepository repo;
 
+    private final ModelMapper mapper; // this is new to me
+
     @Autowired
-    public ProgrammerService(ProgrammerRepository repo) {
+    public ProgrammerService(ProgrammerRepository repo, ModelMapper mapper) {
         this.repo = repo;
+        this.mapper = mapper;
     }
 
-    public List<Programmer> readProgrammers(){
-        return this.repo.findAll();
+    private ProgrammerDTO mapToDTO(Programmer programmer) { return this.mapper.map(programmer, ProgrammerDTO.class); }
+
+
+    public List<ProgrammerDTO> readProgrammers(){
+        return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public Programmer createProgrammer(Programmer programmer){
-        return this.repo.save(programmer);
+    public ProgrammerDTO createProgrammer(Programmer programmer){
+        Programmer tempProgrammer = this.repo.save(programmer);
+        return this.mapToDTO(tempProgrammer);
+        //return this.repo.save(programmer);
     }
 
-    public Programmer findProgrammerById(Long id){
-        return this.repo.findById(id).orElseThrow(ProgrammerNotFoundException::new);
+    public ProgrammerDTO findProgrammerById(Long id){
+        //return this.repo.findById(id).orElseThrow(ProgrammerNotFoundException::new);
+        return this.mapToDTO(this.repo.findById(id).orElseThrow(ProgrammerNotFoundException::new));
     }
 
-    //name, surname,
-
-    public Programmer updateProgrammer(Long id, Programmer programmer){
-        Programmer update = findProgrammerById(id);
+    public ProgrammerDTO updateProgrammer(Long id, Programmer programmer){
+        //Programmer update = findProgrammerById(id);
+        Programmer update = this.repo.findById(id).orElseThrow(ProgrammerNotFoundException::new);
         update.setName(programmer.getName());
         update.setSurname(programmer.getSurname());
         update.setAge(programmer.getAge());
         update.setJobroll(programmer.getJobroll());
-        return this.repo.save(update);
+        Programmer tempProgrammer = this.repo.save(programmer);
+        return this.mapToDTO(tempProgrammer);
     }
-
     public boolean deleteProgrammer(Long id){
         if(!this.repo.existsById(id)){
             throw new ProgrammerNotFoundException();
@@ -48,7 +60,5 @@ public class ProgrammerService {
         this.repo.deleteById(id);
         return this.repo.existsById(id);
     }
-
-
 }
 
