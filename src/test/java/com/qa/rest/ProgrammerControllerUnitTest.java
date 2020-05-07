e463d285a14fdcd4affe88605ca7c93bd49efaa8
MatthewBurtQA <matthewaaronburt@gmail.com>
2020-05-07 14:57:24 +0100

    moved onto testing for moment, set up imports for first junit test, junit testing on ProgrammerController

---
+++ src/test/java/com/qa/rest/ProgrammerControllerUnitTest.java
@@ -0,0 +1,28 @@
+package com.qa.rest;
+
+import com.qa.domain.Programmer;
+import com.qa.dto.ProgrammerDTO;
+import com.qa.service.ProgrammerService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.*;
+
+
+
+public class ProgrammerControllerUnitTest {
+
+}
