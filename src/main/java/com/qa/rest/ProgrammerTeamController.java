5616c09c2758027fbe0d4fc605b11e06bfc981d5
MatthewBurtQA <matthewaaronburt@gmail.com>
2020-05-07 10:12:14 +0100

    now that we're moving into full workflow, switching to feature branch modle

--- src/main/java/com/qa/rest/ProgrammerTeamController.java
+++ src/main/java/com/qa/rest/ProgrammerTeamController.java
@@ -1,8 +1,11 @@
 package com.qa.rest;
 
 import com.qa.domain.ProgrammerTeam;
+import com.qa.dto.ProgrammerTeamDTO;
 import com.qa.service.ProgrammerTeamService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 import java.util.List;
@@ -18,24 +21,46 @@ public class ProgrammerTeamController {
     }
 
     @GetMapping("/getAllProgrammerTeams")
-    public List<ProgrammerTeam> getAllProgrammers(){
-        return this.service.readProgrammerTeams();
+   // public List<ProgrammerTeam> getAllProgrammers(){
+     //   return this.service.readProgrammerTeams();
+    //}
+    public ResponseEntity<List<ProgrammerTeamDTO>> getAllProgrammers() {
+        return ResponseEntity.ok(this.service.readProgrammerTeams());
     }
 
     @PostMapping("/createProgrammerTeam")
-    public ProgrammerTeam createProgrammer(@RequestBody ProgrammerTeam programmer){
-        return this.service.createProgrammerTeam(programmer);
+   public ResponseEntity<ProgrammerTeamDTO> createProgrammer(@RequestBody ProgrammerTeam programmer){
+        return new ResponseEntity<ProgrammerTeamDTO>(this.service.createProgrammerTeam(programmer), HttpStatus.CREATED)
     }
 
+   // public ProgrammerTeam createProgrammer(@RequestBody ProgrammerTeam programmer){
+  //      return this.service.createProgrammerTeam(programmer);
+   // }
+
     @DeleteMapping("/deleteProgrammerTeam/{id}")
-    public boolean deleteProgrammer(@PathVariable Long id){
-        return this.service.deleteProgrammerTeam(id);
+    //public boolean deleteProgrammer(@PathVariable Long id){
+      //  return this.service.deleteProgrammerTeam(id);
+    //}
+    public ResponseEntity<?> deleteProgrammer(@PathVariable Long id){
+    return this.service.deleteProgrammerTeam(id)
+            ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+            : ResponseEntity.noContent().build();
     }
 
     @GetMapping("/getProgrammerTeamById/{id}")
-    public ProgrammerTeam getProgrammerTeamById(@PathVariable Long id, @RequestBody ProgrammerTeam programmer){
-        return this.service.findProgrammerTeamById(id);
+    public ResponseEntity<ProgrammerTeamDTO> getProgrammerById(@PathVariable Long id){
+        return ResponseEntity.ok(this.service.findProgrammerTeamById(id));
     }
+
+    @PutMapping("/updateProgrammerTeam/{id}")
+    public ResponseEntity<ProgrammerTeamDTO> updateProgrammer(@PathVariable Long id, @RequestBody ProgrammerTeam programmer){
+        return ResponseEntity.ok(this.service.updateProgrammerTeam(id, note));
+    }
+
+
+    //public ProgrammerTeam getProgrammerTeamById(@PathVariable Long id, @RequestBody ProgrammerTeam programmer){
+    //    return this.service.findProgrammerTeamById(id);
+    //}
 
     //@PutMapping("/updateProgrammerTeam/{id}")
     //public ProgrammerTeam updateProgrammer(@PathVariable Long id, @RequestBody ProgrammerTeam programmer){
--- src/main/java/com/qa/service/ProgrammerTeamService.java
+++ src/main/java/com/qa/service/ProgrammerTeamService.java
@@ -2,29 +2,45 @@ package com.qa.service;
 
 import com.qa.domain.Programmer;
 import com.qa.domain.ProgrammerTeam;
+import com.qa.dto.ProgrammerTeamDTO;
 import com.qa.ProgrammerNotFoundException.ProgrammerTeamNotFoundException;
+import com.qa.repo.ProgrammerRepository;
 import com.qa.repo.ProgrammerTeamRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProgrammerTeamService {
 
     private final ProgrammerTeamRepository repo;
 
+    private final ModelMapper mapper;
+
     @Autowired
-    public ProgrammerTeamService(ProgrammerTeamRepository repo) {
+    public ProgrammerTeamService(ProgrammerTeamRepository repo, ModelMapper mapper) {
         this.repo = repo;
+        this.mapper = mapper;
     }
 
-    public List<ProgrammerTeam> readProgrammerTeams() {
-        return this.repo.findAll();
+    private ProgrammerTeamDTO mapToDTO(ProgrammerTeam programmerTeam) {
+        return this.mapper.map(programmerTeam, ProgrammerTeamDTO.class);
     }
 
-    public ProgrammerTeam createProgrammerTeam(ProgrammerTeam programmerTeam) {
-        return this.repo.save(programmerTeam);
+    public List<ProgrammerTeamDTO> readProgrammerTeams()
+    {
+        return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+
+        //return this.repo.findAll();
+    }
+
+    public ProgrammerTeamDTO createProgrammerTeam(ProgrammerTeam programmerTeam) {
+
+        //return this.repo.save(programmerTeam);
+        return this.mapToDTO(this.repo.save(programmerTeam));
     }
 
     public ProgrammerTeam findProgrammerTeamById(Long id) {
@@ -43,6 +59,10 @@ public class ProgrammerTeamService {
         }
         this.repo.deleteById(id);
         return this.repo.existsById(id);
+    }
+
+    public ProgrammerTeamDTO addProgrammerToProgrammerTeam(Long id, Programmer programmer){
+        ProgrammerTeam progTeam = this.ProgrammerTeamRepository.findByID(id).orElseThrow()
     }
 
 }
